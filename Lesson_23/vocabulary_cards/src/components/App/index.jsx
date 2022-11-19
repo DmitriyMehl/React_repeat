c4dabf4091f15ee4cca54cb7b17348930295b5a3
unknown <dimamehl18@gmail.com>
2022-11-19 20:18:45 +0100

    triggers components added

--- Lesson_23/vocabulary_cards/src/components/App/index.jsx
+++ Lesson_23/vocabulary_cards/src/components/App/index.jsx
@@ -1,12 +1,14 @@
 import { words } from "../../data/words"
 import CardsContainer from "../CardsContainer";
 import { Context } from "../../context"
+import Triggers from "../Triggers";
 
 function App() {
   return (
     <div>
       <Context.Provider value={{ words }}>
         <CardsContainer />
+        <Triggers />
       </Context.Provider>
     </div>
   );
