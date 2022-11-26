3ba158ebbcc1c3255c96fcc513d39c469efa9d57
unknown <dimamehl18@gmail.com>
2022-11-26 20:48:16 +0100

    srvices section added

--- Lesson_25/site_axample/src/components/Services/index.jsx
+++ Lesson_25/site_axample/src/components/Services/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { servicesData } from "../../data/servicesData"
 import ServiceCard from '../ServiceCard'
+import s from "./index.module.css"
 
 export default function Services() {
   return (
-    <div className={["wrapper"].join(" ")}>
-        {
-            servicesData.map(el => <ServiceCard key={el.id} {...el} />)
-        }
-    </div>
+    <section className={s.service_section}>
+        <div className={["wrapper", s.service_block].join(" ")}>
+            <p className='subheader'>Наши услуги</p>
+            <h2>Мы специализируемся</h2>
+            <div className={s.cards_container}>
+                {
+                    servicesData.map(el => <ServiceCard key={el.id} {...el} />)
+                }
+            </div>
+            
+        </div>
+
+    </section>
   )
 }
